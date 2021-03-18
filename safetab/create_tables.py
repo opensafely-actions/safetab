08133bb7a47118499b9774213aa7766c9d1005b7
CarolineMorton <carolinemorton@gmx.com>
2021-03-18 18:42:32 +0000

    add redaction function and load data in df with check functions, with unit tests

--- safetab/create_tables.py
+++ safetab/create_tables.py
@@ -10,28 +10,66 @@ def import_data(variable_json, data="output/input.csv"):
     # load the data into a pandas DataFame
     test_df = pd.read_csv(data)
 
+    # checks that the variables defined in the json are column names in the
+    # csv and raises an error if note
     for var_item in variable_json['keyword_args']['2_way_tabs']["variables"]:
-        print(var_item)
-#         if var_item in test_df.columns:
-#             pass
-#         else:
-#             raise ImportActionError
+        if var_item not in test_df.columns.values:
+            raise ImportActionError
 
+    # returns the csv
     return test_df
 
-def process_table_request(variables, percent_direction, groupby=False):
+def check_for_low_numbers(table, small_no_limit=5):
+    """
+    Takes in a dataframe such as 2x2 contigency table and checks each value
+    to see if any cell values are small numbers.
+
+    Args:
+        table (Dataframe): table to be redacted
+        small_no_limit (int): the number at which values should be redacted. Defaults to
+            5.
+
+    returns
+        boolean (redaction_needed): True if needs redaction, False if not.
+    """
+    # Convert table into list for iteration
+    all_values = table.values.tolist()
+
+    # set condition_met to False to start with. This changes if limit breached
+    condition_met = False
+
+    # check if retraction needs to occur based on small_no_limit. Default is 5.
+    for column in all_values:
+        for item in column:
+            if type(item) == str:
+                pass
+            elif item <= small_no_limit:
+                condition_met = True
+
+    return condition_met
+
+
+def process_table_request(df, variables, percent_direction=False, groupby=False, small_no_limit=5):
     """
     Take one table request and processes. It creates the table, and if
     cell counts 5 or less in any cell, it redacts the whole table. It keeps
     the title of the table.
     """
-    pass
+    # Make crosstab table
+    table = pd.crosstab(df[variables[0]], df[variables[1]], margins=True, margins_name="Total")
+
+
+
+    return condition_met
 
 def output_tables(output_dir):
     """
     Takes the list of requests for various table configurations, and processes them
     by calling process_table_request() on each request.
     """
+
+    # Load data by calling import_data() function
+
     pass
 
 
--- tests/test_create_tables.py
+++ tests/test_create_tables.py
@@ -1,19 +1,61 @@
-import json
-# import pandas as pd
-from safetab.create_tables import import_data
+import pandas as pd
+import pytest
+from safetab.errors import ImportActionError
 
-test_dict = {"keyword_args":
+from safetab.create_tables import import_data, process_table_request, check_for_low_numbers
+
+correct_json_dict = {"keyword_args":
                 {"2_way_tabs":
-                    {"variables":["sex","age","copd","death"],
+                    {"variables":["sex","ageband","copd","death"],
                     "percentages":"row"}}}
 
+bad_json_dict = {"keyword_args":
+                {"2_way_tabs":
+                    {"variables":["sex","test","copd","death"],
+                    "percentages":"row"}}}
 
 def test_import_data():
     # Import test data
-    test = import_data(test_dict, data="test_data/test_data.csv")
+    test = import_data(correct_json_dict, data="test_data/test_data.csv")
 
     # Check expected 20 rows in dataframe of test data
     assert(len(test.index) == 20)
 
     # Check column names match
+    assert(list(test.columns.values) == ['patient_id','sex', "ageband", "copd", "death"])
+
+    # Checks if raises an error if json does not match the csv columns.
+    with pytest.raises(ImportActionError):
+        test2 = import_data(bad_json_dict, data="test_data/test_data.csv")
+
+
+def test_check_for_low_numbers():
+    no_redact_data = {'test_col_1': [10, 20], 'test_col_2': [20, 40]}
+    no_redact_df = pd.DataFrame(data=no_redact_data)
+
+    # use check_for_low_numbers() on a dataframe that does not require redaction
+    result = check_for_low_numbers(table=no_redact_df, small_no_limit=5)
+    assert(result == False)
+
+    need_redact_data = {'test_col_1': [1, 10], 'test_col_2': [20, 40]}
+    need_redact_df = pd.DataFrame(data=need_redact_data)
+
+    # use check_for_low_numbers() on a dataframe that does require redaction
+    result2 = check_for_low_numbers(table=need_redact_df, small_no_limit=5)
+    assert(result2 == True)
+
+
+def test_process_table_request():
+    test_variables = ['sex', 'death']
+    percentage_direction = "row"
+
+    # Import test data
+    test = import_data(correct_json_dict, data="test_data/test_data.csv")
+    test_table = process_table_request(test, variables=['sex', 'copd'])
+
+    asser
+
+
+
+
 
