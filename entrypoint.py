b0a6929c349b4a43a5c31dd43b8675759df0b3b9
CarolineMorton <carolinemorton@gmx.com>
2021-03-30 14:11:52 +0100

    simplify

--- entrypoint.py
+++ entrypoint.py
@@ -27,28 +27,20 @@ def main():
     # make args parser
     parser = argparse.ArgumentParser(description="Creates tables and redacts small numbers")
 
-    # make subparser
-    subparsers = parser.add_subparsers(
-        title="available commands", description="", metavar="COMMAND")
-    
     # version
     parser.add_argument("--version", action='version', version="safetab 0.0.1")
     
-    # make safetab action
-    parser_make_tabs = subparsers.add_parser(
-        "safetab",
-        help="add some help here"
-    )
-    parser_make_tabs.add_argument("--config", type=str, help="The yaml str passed in")
-    parser_make_tabs.set_defaults(function=make_tables)
+    parser.add_argument("--config", type=str, help="The yaml str passed in")
 
     # parse args
     args = parser.parse_args()
     
     kwargs = vars(args)
-    function = kwargs.pop("function")
     yaml_str = kwargs.pop("config")
-    success = function(yaml_str)
+
+    # check the files exists
+    make_tables(yaml_str)
+    
 
 if __name__ == "__main__":
     main()
