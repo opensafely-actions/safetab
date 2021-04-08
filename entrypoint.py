88bff05d36f685bf8ff6ac3ed23ac303b228baf5
CarolineMorton <carolinemorton@gmx.com>
2021-04-08 17:03:14 +0100

    add better docstrings

--- entrypoint.py
+++ entrypoint.py
@@ -7,6 +7,26 @@ from safetab.create_tables import output_tables
 
 
 def make_tables(input_files, table_config):
+    """
+    :param input_files: The input csvs that the tables are created. These csvs must contain the columns
+        names of the table configuration
+    :param table_config: the table configuration as a json object.
+    :return: a folder per csv which itself contains directories of each table specified. Each folder for
+        the csv contains a table log text file showing redaction, and the subfolders contain the actual tables
+    
+        E.g. for input_one.csv and table configurations of 2-way-simple and groupby-by-copd, you end up
+            with a structure of:
+            
+             - input_one_tables/
+                - 2-way-simple/
+                - group-by-copd
+                - table-log.txt
+             - input_two_tables/
+                - 2-way-simple/
+                - group-by-copd
+                - table-log.txt
+                
+    """
     
     for input_file in input_files:
         output_file_name = os.path.splitext(input_file)[0]
@@ -16,6 +36,11 @@ def make_tables(input_files, table_config):
 
 
 def main():
+    """
+    Command line tool for running safe tab. Config takes in an argument of a json string which
+        has a pointer for input csvs and table configurations. These arguments are parsed to
+        make_tables() function.
+    """
     # make args parser
     parser = argparse.ArgumentParser(description="Creates tables and redacts small numbers")
 
