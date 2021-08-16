7211a9450e6592f136c3bf72865d16ca0ba8198b
Iain Dillingham <iain@dillingham.me.uk>
2021-08-16 11:31:55 +0100

    Refactor redaction function
    The existing logic isn't as effective as it could be, because it doesn't
    break early. However, we could simply refactor it away.

--- action/redaction_tools.py
+++ action/redaction_tools.py
@@ -6,21 +6,7 @@ import pandas as pd
 
 def contains_small_numbers(table: pd.DataFrame, threshold: int = 5) -> bool:
     """Does `table` contain numbers less than or equal to `threshold`?"""
-    # Convert table into list for iteration
-    all_values = table.values.tolist()
-
-    # set condition_met to False to start with. This changes if limit breached
-    condition_met = False
-
-    # check if retraction needs to occur based on threshold. Default is 5.
-    for column in all_values:
-        for item in column:
-            if isinstance(item, str):
-                pass
-            elif item <= threshold:
-                condition_met = True
-
-    return condition_met
+    return table.le(threshold).any(axis=None)
 
 
 def process_table_request(df: pd.DataFrame, variables: Dict, small_no_limit: int = 5):
