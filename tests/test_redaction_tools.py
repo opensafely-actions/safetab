8072d1bfa58412ea704954179609aa3ed9954589
Iain Dillingham <iain@dillingham.me.uk>
2021-08-16 15:41:02 +0100

    Replace fixture file with fixture function
    Fixture files are brittle; the mechanism that generated the data and the
    data themselves are decoupled. Let's replace them with fixture
    functions.

--- tests/test_redaction_tools.py
+++ tests/test_redaction_tools.py
@@ -1,16 +1,9 @@
+import itertools
+
 import pandas as pd
+from pandas import testing
 
-from action.find_save_tools import import_data
 from action.redaction_tools import contains_small_numbers, process_table_request
-
-TEST_DATA_CSV = "tests/test_data/test_data.csv"
-
-correct_json_dict = {
-    "simple_2_way_tabs": {
-        "tab_type": "2-way",
-        "variables": ["sex", "ageband", "copd", "death"],
-    }
-}
 
 
 class TestContainsSmallNumbers:
@@ -23,27 +16,35 @@ class TestContainsSmallNumbers:
         assert not contains_small_numbers(table)
 
 
-def test_process_table_request():
-    no_redaction_needed_variables = ["sex", "copd"]
-    redaction_needed_variables = ["ageband", "sex"]
+class TestMakeCrosstab:
+    @staticmethod
+    def table_factory(num_repeated_rows):
+        return pd.DataFrame(
+            list(itertools.product(["F", "M"], [0, 1])) * num_repeated_rows,
+            columns=("sex", "has_condition"),
+        )
 
-    # Import test data
-    test = import_data(correct_json_dict, data=TEST_DATA_CSV)
+    def test_contains_small_numbers(self):
+        table = self.table_factory(5)
+        cols = list(table.columns)
 
-    # pick 2 variables which have sufficient numbers to require no redaction
-    variables, test_table = process_table_request(
-        test, variables=no_redaction_needed_variables
-    )
+        obs_cols, obs_table = process_table_request(table, cols)
+        exp_cols, exp_table = cols, "REDACTED"
 
-    # check values are correct
-    # first row should be female sex. There are 16 women with copd, 16 without
-    assert test_table.iloc[0][0] == 16
+        assert obs_cols == exp_cols
+        assert obs_table == exp_table
 
-    # pick 2 variables which have sufficient numbers to require no redaction
-    variables2, test_table_2 = process_table_request(
-        test, variables=redaction_needed_variables
-    )
+    def test_does_not_contain_small_numbers(self):
+        table = self.table_factory(6)
+        cols = list(table.columns)
 
-    # assert redacted
-    # print("TABLE 2: ", test_table_2)
-    assert test_table_2 == "REDACTED"
+        obs_cols, obs_table = process_table_request(table, cols)
+        exp_cols = cols
+        exp_table = pd.DataFrame(
+            [[6, 6], [6, 6]],
+            index=pd.Index(["F", "M"], name="sex"),
+            columns=pd.Index([0, 1], name="has_condition"),
+        )
+
+        assert obs_cols == exp_cols
+        testing.assert_frame_equal(obs_table, exp_table)
